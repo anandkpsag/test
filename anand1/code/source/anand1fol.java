32e34ee0230c199cd30cfcf1b7d22ffc18b6cbe2
saguser <saguser@VM101SD2.eur.ad.sag>
2020-12-18 01:25:33 -0800

    commit

---
+++ anand1/code/source/anand1fol.java
@@ -0,0 +1,42 @@
+
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+import com.google.gson.Gson;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class anand1fol
+
+{
+	// ---( internal utility methods )---
+
+	final static anand1fol _instance = new anand1fol();
+
+	static anand1fol _newInstance() { return new anand1fol(); }
+
+	static anand1fol _cast(Object o) { return (anand1fol)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void js (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(js)>> ---
+		// @sigtype java 3.5
+		Gson gson = new Gson();
+		
+			
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
