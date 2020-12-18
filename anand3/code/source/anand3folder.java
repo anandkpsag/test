c95c46f9b1925dc2b28da79edfb3df2fdb26253f
saguser <saguser@VM101SD2.eur.ad.sag>
2020-12-18 04:49:07 -0800

    c1

---
+++ anand3/code/source/anand3folder.java
@@ -0,0 +1,44 @@
+
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+import com.google.gson.Gson;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class anand3folder
+
+{
+	// ---( internal utility methods )---
+
+	final static anand3folder _instance = new anand3folder();
+
+	static anand3folder _newInstance() { return new anand3folder(); }
+
+	static anand3folder _cast(Object o) { return (anand3folder)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void new_javaService (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(new_javaService)>> ---
+		// @sigtype java 3.5
+		
+		
+		Gson gson = new Gson();
+		
+			
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
