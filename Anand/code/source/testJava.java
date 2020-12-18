289f64bf8b9aa3668b849deaa602086e83311b7e
saguser <saguser@VM101SD2.eur.ad.sag>
2020-12-18 00:12:31 -0800

    commit

---
+++ Anand/code/source/testJava.java
@@ -0,0 +1,40 @@
+
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+import com.google.gson.Gson;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class testJava
+
+{
+	// ---( internal utility methods )---
+
+	final static testJava _instance = new testJava();
+
+	static testJava _newInstance() { return new testJava(); }
+
+	static testJava _cast(Object o) { return (testJava)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void java1 (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(java1)>> ---
+		// @sigtype java 3.5
+		Gson gson = new Gson();
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
