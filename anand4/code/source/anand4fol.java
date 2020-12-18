2c0b8f1070dfc895ed6a62a9d30eb9a16447b3f9
saguser <saguser@VM101SD2.eur.ad.sag>
2020-12-18 05:13:18 -0800

    c1

---
+++ anand4/code/source/anand4fol.java
@@ -0,0 +1,41 @@
+
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+import com.google.gson.Gson;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class anand4fol
+
+{
+	// ---( internal utility methods )---
+
+	final static anand4fol _instance = new anand4fol();
+
+	static anand4fol _newInstance() { return new anand4fol(); }
+
+	static anand4fol _cast(Object o) { return (anand4fol)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void js44 (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(js44)>> ---
+		// @sigtype java 3.5
+		Gson gson = new Gson();
+			
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
