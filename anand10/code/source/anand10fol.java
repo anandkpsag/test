a77a1c03c967f25f3ba28ae28b84a3c60f4c87ee
saguser <saguser@VM101SD2.eur.ad.sag>
2020-12-21 03:02:55 -0800

    c33

---
+++ anand10/code/source/anand10fol.java
@@ -0,0 +1,42 @@
+
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+import com.google.gson.Gson;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class anand10fol
+
+{
+	// ---( internal utility methods )---
+
+	final static anand10fol _instance = new anand10fol();
+
+	static anand10fol _newInstance() { return new anand10fol(); }
+
+	static anand10fol _cast(Object o) { return (anand10fol)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void js10 (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(js10)>> ---
+		// @sigtype java 3.5
+			
+		Gson gson = new Gson();
+			
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
