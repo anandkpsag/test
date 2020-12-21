e74d50f6fe581de73b568666a1e52f899efddc39
saguser <saguser@VM101SD2.eur.ad.sag>
2020-12-21 01:54:46 -0800

    c11

---
+++ anand6/code/source/anand6fol.java
@@ -0,0 +1,42 @@
+
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+import com.google.gson.Gson;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class anand6fol
+
+{
+	// ---( internal utility methods )---
+
+	final static anand6fol _instance = new anand6fol();
+
+	static anand6fol _newInstance() { return new anand6fol(); }
+
+	static anand6fol _cast(Object o) { return (anand6fol)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void js6 (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(js6)>> ---
+		// @sigtype java 3.5
+		
+		Gson gson= new Gson();
+			
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
