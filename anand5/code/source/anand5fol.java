8904f628fe42dfc08b58f7e3e7d30d2d87dab8e3
saguser <saguser@VM101SD2.eur.ad.sag>
2020-12-21 01:48:37 -0800

    c1

---
+++ anand5/code/source/anand5fol.java
@@ -0,0 +1,41 @@
+
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+import com.google.gson.Gson;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class anand5fol
+
+{
+	// ---( internal utility methods )---
+
+	final static anand5fol _instance = new anand5fol();
+
+	static anand5fol _newInstance() { return new anand5fol(); }
+
+	static anand5fol _cast(Object o) { return (anand5fol)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void j5 (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(j5)>> ---
+		// @sigtype java 3.5
+		Gson gson = new Gson();
+			
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
