8b765b75d019445375587cd4a86ea6d0a7a29870
saguser <saguser@VM101SD2.eur.ad.sag>
2020-12-21 02:19:30 -0800

    c2

---
+++ anand7/code/source/anand7fol.java
@@ -0,0 +1,41 @@
+
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+import com.google.gson.Gson;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class anand7fol
+
+{
+	// ---( internal utility methods )---
+
+	final static anand7fol _instance = new anand7fol();
+
+	static anand7fol _newInstance() { return new anand7fol(); }
+
+	static anand7fol _cast(Object o) { return (anand7fol)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void js7 (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(js7)>> ---
+		// @sigtype java 3.5
+		Gson gson = new Gson();
+			
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
