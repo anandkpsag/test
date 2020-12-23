7e0fa89ad7cde8fcf6fbed2305e9147b51bf5c25
saguser <saguser@VM101SD2.eur.ad.sag>
2020-12-23 04:32:05 -0800

    c12

---
+++ anand12/code/source/anand12fol.java
@@ -0,0 +1,41 @@
+
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+import com.google.gson.Gson;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class anand12fol
+
+{
+	// ---( internal utility methods )---
+
+	final static anand12fol _instance = new anand12fol();
+
+	static anand12fol _newInstance() { return new anand12fol(); }
+
+	static anand12fol _cast(Object o) { return (anand12fol)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void j12 (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(j12)>> ---
+		// @sigtype java 3.5
+		Gson gson = new Gson();
+			
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
---
+++ anand12/ns/anand12fol/j12/java.frag
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<Values version="2.0">
+  <value name="name">j12</value>
+  <value name="encodeutf8">true</value>
+  <value name="body">R3NvbiBnc29uID0gbmV3IEdzb24oKTsNCgk=</value>
+</Values>
