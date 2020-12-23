97949f37a51c62fca3208d51dd6c2d4f7f6ebbd8
bhre <bhre@SAG-2F0X1Z2>
2020-12-23 14:55:55 +0530

    Adding new add Ints project...

---
+++ AddInegers/code/source/addVerify.java
@@ -0,0 +1,38 @@
+
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class addVerify
+
+{
+	// ---( internal utility methods )---
+
+	final static addVerify _instance = new addVerify();
+
+	static addVerify _newInstance() { return new addVerify(); }
+
+	static addVerify _cast(Object o) { return (addVerify)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void addInts (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(addInts)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
