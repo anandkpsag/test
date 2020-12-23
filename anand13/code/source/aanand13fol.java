e20cf1e867798833cea42e3cccfb08df55fb4f1a
saguser <saguser@VM101SD2.eur.ad.sag>
2020-12-23 06:19:33 -0800

    c13

---
+++ anand13/code/source/aanand13fol.java
@@ -0,0 +1,38 @@
+
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class aanand13fol
+
+{
+	// ---( internal utility methods )---
+
+	final static aanand13fol _instance = new aanand13fol();
+
+	static aanand13fol _newInstance() { return new aanand13fol(); }
+
+	static aanand13fol _cast(Object o) { return (aanand13fol)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void js13 (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(js13)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
