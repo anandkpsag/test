5e40e25e9f363522fa62804ba0013d6def2c47f0
saguser <saguser@VM101SD2.eur.ad.sag>
2020-12-23 06:41:18 -0800

    c14

---
+++ anand14/code/source/anand14fol.java
@@ -0,0 +1,38 @@
+
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class anand14fol
+
+{
+	// ---( internal utility methods )---
+
+	final static anand14fol _instance = new anand14fol();
+
+	static anand14fol _newInstance() { return new anand14fol(); }
+
+	static anand14fol _cast(Object o) { return (anand14fol)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void js14 (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(js14)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
