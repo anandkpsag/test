54c96423f133156ccae0d6a56728df62b6ce5868
saguser <saguser@VM101SD2.eur.ad.sag>
2021-01-05 23:46:33 -0800

    c4

---
+++ wel-130/code/source/apple.java
@@ -0,0 +1,38 @@
+
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class apple
+
+{
+	// ---( internal utility methods )---
+
+	final static apple _instance = new apple();
+
+	static apple _newInstance() { return new apple(); }
+
+	static apple _cast(Object o) { return (apple)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void js130 (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(js130)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
