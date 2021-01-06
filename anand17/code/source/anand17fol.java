2b229debcfa5f065a68b18f9d52e63cde2d31825
saguser <saguser@VM101SD2.eur.ad.sag>
2021-01-05 23:08:22 -0800

    c2

---
+++ anand17/code/source/anand17fol.java
@@ -0,0 +1,41 @@
+
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+import com.google.gson.Gson;
+// --- <<IS-END-IMPORTS>> ---
+
+public final class anand17fol
+
+{
+	// ---( internal utility methods )---
+
+	final static anand17fol _instance = new anand17fol();
+
+	static anand17fol _newInstance() { return new anand17fol(); }
+
+	static anand17fol _cast(Object o) { return (anand17fol)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void js17 (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(js17)>> ---
+		// @sigtype java 3.5
+		Gson gson = new Gson();
+			
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
